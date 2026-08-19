1ffd6e43fd6995a85f3a6949a60c168b6b9b851e
Aholic <121384281+gfk-sveyigey@users.noreply.github.com>
2026-08-19 15:20:17 +0800

    更新 fakebc.js

--- fakebc.js
+++ fakebc.js
@@ -1,11 +1,16 @@
-// 匹配包含"ad"的请求，返回空JSON
+// fakebc.js - 针对 bad.com 的请求处理
 if ($request.url.indexOf("ad") !== -1) {
+  // 拦截请求，返回空数据或自定义响应
   $done({
     status: 200,
     headers: {
       "Content-Type": "application/json"
     },
-    body: "{}"
+    body: JSON.stringify({ 
+      code: 0, 
+      msg: "Request blocked by QX script",
+      data: null 
+    })
   });
 }
 $done({});
