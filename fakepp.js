7491227e8fb8880bfb577440906eeadd74a36b19
Aholic <121384281+gfk-sveyigey@users.noreply.github.com>
2026-08-22 12:26:22 +0800

    创建 fakepp.js

---
+++ fakepp.js
@@ -0,0 +1,37 @@
+//百层深渊本地存档备份及读档替换。
+
+
+if ($request.path === "/push") {
+    try {
+        const reqBody = JSON.parse($request.body)
+        const archive = reqBody.text
+        $prefs.setValueForKey(archive, "archive")
+        $notify("百层深渊", "存档", "本地备份已更新。")
+    } catch (e) {
+        $notify("百层深渊", "存档", "本地备份失败：" + e.message)
+    }
+    $done({})
+}
+
+if ($request.path === "/pull") {
+    try {
+        const resBody = JSON.parse($response.body)
+        if (resBody.errorCode && resBody.errorCode != 0) {
+            throw new Error("在线存档异常。")
+        }
+        const archive = $prefs.valueForKey("archive")
+        if (archive) {
+            resBody.text = archive
+            $notify("百层深渊", "读档", "已替换为本地存档。")
+        } else {
+            $notify("百层深渊", "读档", "无本地存档，仍读取在线存档。")
+        }			
+        $done({body: JSON.stringify(resBody)})
+    } catch (e) {
+        $notify("百层深渊", "读档", "读档失败: " + e.message)
+        $done({})
+    }
+    return
+}
+
+$done({})
